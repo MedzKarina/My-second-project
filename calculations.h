f69947496d883e91a4a753ae22482380f029718a
MedzKarina <91346039+MedzKarina@users.noreply.github.com>
2022-11-16 19:36:21 +0200

    Create calculations.h

---
+++ calculations.h
@@ -0,0 +1,18 @@
+#ifndef CALCULATIONS_H_INCLUDED
+#define CALCULATIONS_H_INCLUDED
+
+#include <string.h>
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <iomanip>
+#include <bits/stdc++.h>
+#include <ctime>
+#include <fstream>
+using std::vector;
+using namespace std;
+
+float Galutinis(int egz, vector<int> paz);
+int Mediana(vector<int> arr);
+
+#endif
