268af7483ec2bad8ee1417fd6fedb52619796aa4
MedzKarina <91346039+MedzKarina@users.noreply.github.com>
2022-11-17 12:56:48 +0200

    Create student.h

---
+++ student.h
@@ -0,0 +1,41 @@
+#include "calculations.h"
+#ifndef STUDENT_H_INCLUDED
+#define STUDENT_H_INCLUDED
+
+#include <string.h>
+#include <iostream>
+#include <vector>
+#include <list>
+#include <sstream>
+#include <iomanip>
+#include <bits/stdc++.h>
+#include <ctime>
+#include <fstream>
+#include <iterator>
+#include <algorithm>
+using std::vector;
+using namespace std;
+
+struct studentas {
+    string vardas;
+    string pavarde;
+    vector<int> paz;
+    int egz;
+    float galutinis;
+    int mediana;
+};
+
+bool lyginimas(studentas i1, studentas i2);
+string ToString(int value);
+void Timer(clock_t begin, clock_t end);
+void Isvedimas(vector<studentas> studentai, string fileName);
+bool Grupavimas(vector<studentas> studentai);
+void Grupavimas2(vector<studentas> studentai);
+void Grupavimasl(list<studentas> studentai);
+void Grupavimasl2(list<studentas> studentai);
+studentas Gen(int cnd, int studentoId);
+vector<studentas> ReadFromFile (string fileName);
+list<studentas> ReadFromFileList (string fileName);
+void Gen2(int gstud);
+
+#endif
